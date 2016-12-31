543fd4a1e25e5823e7d8df00f2d35dbdcb09e756
Justin Besteman <justin@besteman.io>
2016-12-30 21:18:11 -0500

    Finished Else if statement

--- assets/js/td.js
+++ assets/js/td.js
@@ -36,7 +36,7 @@ $(document).ready(function(){
 		div_head.appendTo(li);
 
 		// JavaScript Foundations Subjects
-		var jf_subjects = ["Variables" , "If Statement" , "Else Statement" , "Else Statement" , "Else If Statement"];
+		var jf_subjects = ["Variables" , "If Statement"  , "Else Statement" , "Else If Statement"];
 
 		for (var i = 0; i < jf_subjects.length; i++) {
 			
@@ -84,10 +84,10 @@ $(document).ready(function(){
 					PDFObject.embed("assets/latex/if/if.pdf", "#example1");
 					break;
 
-				case "else":
+				case "Else Statement":
 					PDFObject.embed("assets/latex/else/else.pdf", "#example1");
 
-				case "Else Statement":
+				case "Else If Statement":
 
 					break;
 				case "elseif":
@@ -99,226 +99,6 @@ $(document).ready(function(){
 		}); // End of On Click Function
 
 
-
-	$("#jQuery").click(function(){
-
-		
-
-	}); // End of jQuery Click Event
-
-
-
 }); // End of Document Ready 
 
 
-// function vars() {
-
-// } // End of Vars Function
-
-// function if_statement(){
-
-
-
-// 	$(".display").html("");
-
-// 	var div = $("<div>");
-// 	var div_ex1 = $("<div>");
-// 	var div_ex2 = $("<div>");
-// 	var div_ex3 = $("<div>");
-
-// 	div.attr({
-// 		"class": "center-align"
-// 	});
-
-// 	div_ex1.attr({
-// 		"class": "col s3"
-// 	});
-
-// 	div_ex1.attr({
-// 		"class": "col s3"
-// 	});
-
-// 		div_ex1.attr({
-// 		"class": "col s3"
-// 	});
-
-// 	var text = "<h3>If Statement</h3><h4>Block Level Code</h4><p>HAS to take a parameter</p><p>Does not need a else statement unless needed</p><p>Start with key work 'if'</p><p>Then () this is where the parameter is entered</p><p>Then curly braces </p>"; 
-
-
-// 	var text_ex1 = "<h5>Example 1</h5> \
-// 					<br> \
-// 					<p><font color='red'>if</font> (<font color='blue'>5</font> <font color='red'>==</font> <font color='blue'>5</font>) { </p> \
-// 					<br> \
-// 					<p> &nbsp &nbsp &nbsp	// If the condition is met or true in the ()</p> \
-// 					<p>	&nbsp &nbsp &nbsp // will do the logic inside the curly brace</p> \
-// 					<br> \
-// 					<p>	&nbsp &nbsp &nbsp console.log('<font color='orange'>They are equal</font>');</p> \
-// 					} // End of If Statement" ;
-
-// 	div.append(text);
-// 	div_ex1.append(text_ex1);
-
-// 	$(".display").html(div);
-// 	$(".display").append(div_ex1);
-
-// } // End of If Statement Function
-
-
-// function else_statement(){
-
-// 	$(".display").html("");
-
-// 	var div = $("<div>");
-// 	var div_ex1 = $("<div>");
-// 	var div_ex2 = $("<div>");
-// 	var div_ex3 = $("<div>");
-
-// 	div.attr({
-// 		"class": "center-align"
-// 	});
-
-// 	div_ex1.attr({
-// 		"class": "col s4"
-// 	});
-
-// 	div_ex2.attr({
-// 		"class": "col s4"
-// 	});
-
-// 	div_ex3.attr({
-// 		"class": "col s4"
-// 	});
-
-// 	var text = "<h3>Else Statement</h3><h4>Block Level Code</h4><p>Else does NOT take a parameter</p><p>Has to have an if statement attached to it</p><p>Start with an if key work</p><p>After the curly brace for the if statement</p><p>type the key word else</p>"; 
-
-
-// 	var text_ex1 = "<h5>Example 1</h5> \
-// 					<br> \
-// 					<p><font color='red'>if</font> (<font color='blue'>5</font> <font color='red'>==</font> <font color='blue'>6</font>) { </p> \
-// 					<br> \
-// 					<p> &nbsp &nbsp &nbsp	// If the condition is met or true in the ()</p> \
-// 					<p>	&nbsp &nbsp &nbsp // will do the logic inside the curly brace</p> \
-// 					<br> \
-// 					<p>	&nbsp &nbsp &nbsp console.log('<font color='orange'>They are equal</font>');</p> \
-// 					} <font color='red'>else</font> {  \
-// 					<p> &nbsp &nbsp &nbsp	// If the condition is met or true in the ()</p> \
-// 					<p>	&nbsp &nbsp &nbsp // will do the logic inside the curly brace</p> \
-// 					<p>	&nbsp &nbsp &nbsp console.log('<font color='orange'>They are not equal</font>');</p> \
-// 					} // End of If Else Statement";
-
-// 	var text_ex2 = "<h5>Example 2</h5> \
-// 					<br> \
-// 					<p>var <font color='blue'>user</font> = <font color='orange'>'fried eggs'</font>;</p> \
-// 					<p><font color='red'>if</font> (<font color='blue'>user</font> <font color='red'>==</font> <font color='blue'> 'sunny side up'</font>) { </p> \
-// 					<br> \
-// 					<p> &nbsp &nbsp &nbsp	// If the condition is met or true in the ()</p> \
-// 					<p>	&nbsp &nbsp &nbsp // will do the logic inside the curly brace</p> \
-// 					<br> \
-// 					<p>	&nbsp &nbsp &nbsp console.log('<font color='orange'>Make sunny side up for the user</font>');</p> \
-// 					} <font color='red'>else</font> {  \
-// 					<p> &nbsp &nbsp &nbsp	// If the condition is met or true in the ()</p> \
-// 					<p>	&nbsp &nbsp &nbsp // will do the logic inside the curly brace</p> \
-// 					<p>	&nbsp &nbsp &nbsp console.log('<font color='orange'>Make fried eggs for the user</font>');</p> \
-// 					} // End of If Else Statement";
-
-// 	div.append(text);
-// 	div_ex1.append(text_ex1);
-// 	div_ex2.append(text_ex2);
-
-// 	$(".display").html(div);
-// 	$(".display").append(div_ex1);
-// 	$(".display").append(div_ex2);
-
-// } // End of Else Statement
-
-
-// function elseif_statement(){
-
-// 	$(".display").html("");
-
-// 	var div = $("<div>");
-// 	var div_ex1 = $("<div>");
-// 	var div_ex2 = $("<div>");
-// 	var div_ex3 = $("<div>");
-
-// 	div.attr({
-// 		"class": "center-align"
-// 	});
-
-// 	div_ex1.attr({
-// 		"class": "col s4"
-// 	});
-
-// 	div_ex2.attr({
-// 		"class": "col s4"
-// 	});
-
-// 	div_ex3.attr({
-// 		"class": "col s4"
-// 	});
-
-// 	var text = "<h3>Else If Statement</h3><h4>Block Level Code</h4><p>Else if does take a parameter</p><p>Has to have an if statement attached to it</p><p>Start with an if key work</p><p>After the curly brace for the if statement</p><p>type the key word else if</p>"; 
-
-
-// 	var text_ex1 = "<h5>Example 1</h5> \
-// 					<br> \
-// 					<p><font color='red'>if</font> (<font color='blue'>5</font> <font color='red'>==</font> <font color='blue'>6</font>) { </p> \
-// 					<br> \
-// 					<p> &nbsp &nbsp &nbsp	// If the condition is met or true in the ()</p> \
-// 					<p>	&nbsp &nbsp &nbsp // will do the logic inside the curly brace</p> \
-// 					<br> \
-// 					<p>	&nbsp &nbsp &nbsp console.log('<font color='orange'>They are equal</font>');</p> \
-// 					} <font color='red'>else if </font>(<font color='blue'>5</font> <font color='red'>==</font> <font color='blue'>5</font>) {   \
-// 					<p> &nbsp &nbsp &nbsp	// If the condition is met or true in the ()</p> \
-// 					<p>	&nbsp &nbsp &nbsp // will do the logic inside the curly brace</p> \
-// 					<p>	&nbsp &nbsp &nbsp console.log('<font color='orange'>They are equal</font>');</p> \
-// 					} // End of If Else if Statement";
-
-// 	var text_ex2 = "<h5>Example 2</h5> \
-// 					<br> \
-// 					<p>var <font color='blue'>user</font> = <font color='orange'>'fried eggs'</font>;</p> \
-// 					<p><font color='red'>if</font> (<font color='blue'>user</font> <font color='red'>==</font> <font color='blue'> 'sunny side up'</font>) { </p> \
-// 					<br> \
-// 					<p> &nbsp &nbsp &nbsp	// If the condition is met or true in the ()</p> \
-// 					<p>	&nbsp &nbsp &nbsp // will do the logic inside the curly brace</p> \
-// 					<br> \
-// 					<p>	&nbsp &nbsp &nbsp console.log('<font color='orange'>Make sunny side up for the user</font>');</p> \
-// 					} <font color='red'>else if </font>(<font color='blue'>user</font> <font color='red'>==</font> <font color='blue'>'scrambled'</font>) {  \
-// 					<p> &nbsp &nbsp &nbsp	// If the condition is met or true in the ()</p> \
-// 					<p>	&nbsp &nbsp &nbsp // will do the logic inside the curly brace</p> \
-// 					<p>	&nbsp &nbsp &nbsp console.log('<font color='orange'>Make scrambled eggs for the user</font>');</p> \
-// 					} <font color='red'>else if </font>(<font color='blue'>user</font> <font color='red'>==</font> <font color='blue'>'fried eggs'</font>) { \
-// 					<p> &nbsp &nbsp &nbsp	// If the condition is met or true in the ()</p> \
-// 					<p>	&nbsp &nbsp &nbsp // will do the logic inside the curly brace</p> \
-// 					<p>	&nbsp &nbsp &nbsp console.log('<font color='orange'>Make fried eggs for the user</font>');</p> \
-// 					} <font color='red'>else</font> {  \
-// 					<p> &nbsp &nbsp &nbsp	// If the condition is not met or true in the previous ()</p> \
-// 					<p>	&nbsp &nbsp &nbsp // will do the logic inside the curly brace</p> \
-// 					<p>	&nbsp &nbsp &nbsp console.log('<font color='orange'>No eggs for you</font>');</p> \
-// 					} // End of If Else If Else Statement";
-
-// 	div.append(text);
-// 	div_ex1.append(text_ex1);
-// 	div_ex2.append(text_ex2);
-
-// 	$(".display").html(div);
-// 	$(".display").append(div_ex1);
-// 	$(".display").append(div_ex2);
-
- // End of Else if statement function
-
-
-  // <ul class="collapsible" data-collapsible="accordion">
-  //   <li>
-  //     <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
-  //     <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
-  //   </li>
-  //   <li>
-  //     <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
-  //     <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
-  //   </li>
-  //   <li>
-  //     <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-  //     <div class="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
-  //   </li>
-  // </ul>
